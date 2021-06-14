50df8798cda8d23cb91bca57e515e6df5db00432
Cralus <defactopersona@gmail.com>
2021-06-14 13:47:47 +0100

    formatting added to

--- main.js
+++ main.js
@@ -8,18 +8,22 @@ const genContent = () =>{
         {
             message = `A random inpiring message`
         }
+        break;
     case 1:
         {
             message = `A random inspiring message but very, very, very long like really really long`
         }
+        break;
     case 2:
         {
             message = `A random un-inspiring  message`
         }
+        break;
     case 3:
         {
             message = `Man I hate writing messages quite a bit`
         }
+        break;
     case 4:
         {
             message = `Minimum message achieved`
@@ -38,29 +42,75 @@ const genSig = () =>
         {
             sig = `-S. Body`
         }
+        break;
     case 1:
         {
             sig = `-L. Da Vinci`
         }
+        break;
     case 2:
         {
             sig = `-Abraham Lincoln`
         }
+        break;
     case 3:
         {
             sig = `-Me Myself and I`
         }
+        break;
     case 4:
         {
             sig = `-W. Shakespear`
         }
-
+       
     }
     return sig;
 }
 const genFormat = (content, sig) =>
 {
-content = content + `/n` + sig;
+let lineType = '';
+let paddingType = '';
+let rand = Math.floor(Math.random() * 5)
+switch(rand){
+case 0:
+    {
+        lineType = `=`
+        paddingType = '#'
+    }
+    break;
+case 1:
+    {
+        lineType = `-`
+        paddingType = `$`
+    }
+    break;
+case 2:
+    {
+        lineType = `~`
+        paddingType = `+`
+    }
+    break;
+case 3:
+    {
+        lineType = `¬`
+        paddingType = `@`
+    }
+    break;
+case 4:
+    {
+        lineType = `_`
+        paddingType = `|`
+    }
+}
+sig.padEnd((content.length), ' ')
+let fullLine = ''
+fullLine.padStart((content.length + 2), lineType)
+content.padStart((content.length+1), paddingType)
+content.padEnd((content.length+1), paddingType)
+sig.padStart((sig.length+1), paddingType)
+content.padEnd((sig.length+1), paddingType)
+content = fullLine + '\n' + content + `\n` + sig + `\n`;
+
 return content;
 }
 console.log(genFormat(genContent(), genSig()))
